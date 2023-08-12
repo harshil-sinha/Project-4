e850505c4089c9b492e710d999dcc3c31df15d33
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2023-08-12 12:18:17 +0530

    Update Navigation.js

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,29 +1,31 @@
-import React from 'react';
+import React,{useContext} from 'react';
 
 import classes from './Navigation.module.css';
+import AuthContext from '../Store/auth-context';
 
 const Navigation = (props) => {
+    const ctx = useContext(AuthContext)
   return (
-    <nav className={classes.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
+            <nav className={classes.nav}>
+            <ul>
+            {props.isLoggedIn && (
+            <li>
+                <a href="/">Users</a>
+            </li>
         )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
+            {props.isLoggedIn && (
+            <li>
+                <a href="/">Admin</a>
+            </li>
+            )}
+            {props.isLoggedIn && (
+            <li>
+                <button onClick={ctx.onLogout}>Logout</button>
+            </li>
+            )}
       </ul>
     </nav>
-  );
+        );
 };
 
 export default Navigation;
